92cdcd3e00fbcb97e43b8e7172f61aecda3ad17d
Patrick <pakim249@ischool.berkeley.edu>
2021-08-29 22:26:55 -0700

    Adding customization

--- scripts/pet-gotchi.ts
+++ scripts/pet-gotchi.ts
@@ -3,10 +3,13 @@ import {ethers} from "hardhat";
 import utils from "web3-utils";
 import hre from "hardhat";
 
-
 const gameAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
 const GWEI = 1000000000n
 const HOUR = 1000 * 60 * 60;
+
+// Set initial delay and stagger in hours
+const initialDelay = 1
+const stagger = 0.1
 
 function delay(ms: number) {
   return new Promise( resolve => setTimeout(resolve, ms) );
@@ -15,12 +18,13 @@ function delay(ms: number) {
 async function main() {
   const [owner] = await ethers.getSigners();
   console.log("Owner", owner.address);
+  await delay(HOUR * initialDelay)
 
   while(true) {
     const tx = await owner.sendTransaction({to: gameAddress, data: process.env.TX_DATA, gasPrice: 50n * GWEI, gasLimit: 60000});
     console.log(tx.hash);
     console.log("Going to sleep for 12.2 hours");
-    await delay(HOUR * 12.2);
+    await delay(HOUR * 12 + stagger);
   }
 
 }

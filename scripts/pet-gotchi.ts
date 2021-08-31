85c89adb15b30e356a9b27174b40fdb12a2f0241
Patrick <pakim249@ischool.berkeley.edu>
2021-08-30 23:20:26 -0700

    dfsafas

--- scripts/pet-gotchi.ts
+++ scripts/pet-gotchi.ts
@@ -2,13 +2,14 @@ import "@nomiclabs/hardhat-waffle";
 import {ethers} from "hardhat";
 import utils from "web3-utils";
 import hre from "hardhat";
+import { stringify } from "querystring";
 
 const gameAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
 const GWEI = 1000000000n;
 const HOUR = 1000 * 60 * 60;
 
 // Set initial delay and stagger in hours
-const initialDelay = 11.9;
+const initialDelay = 11.8;
 const stagger = 0.1;
 
 function delay(ms: number) {
@@ -18,14 +19,15 @@ function delay(ms: number) {
 async function main() {
   const [owner] = await ethers.getSigners();
   console.log("Owner", owner.address);
+  console.log("Going to sleep for " + String(initialDelay) + " hours");
   await delay(HOUR * initialDelay);
 
   while(true) {
     const tx = await owner.sendTransaction({to: gameAddress, data: process.env.TX_DATA, gasPrice: 50n * GWEI, gasLimit: 60000});
     console.log(tx.hash);
     console.log("Going to sleep for 12 hours");
-
-    await delay(HOUR * 12 + stagger);
+    await delay(10000);
+    //await delay(HOUR * 12 + stagger);
   }
 
 }
